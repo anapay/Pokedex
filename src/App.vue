5dcc3f0fcbbd37154d73c0526b5e79e5f1bd4042
anapay <anapay42@gmail.com>
2020-08-21 08:59:23 -0300

    http requests with axios

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div id="app">
-    
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from "axios";
 export default {
-  name: 'App',
-}
+  name: "App",
+  data(){
+    return {
+      pokemons: []
+    }
+
+  },
+  created: function() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      console.log("lista de pokemons");
+      this.pokemons = res.data.results;
+      console.log(this.pokemons);
+      
+      
+    })
+  }
+};
 </script>
 
 <style>
