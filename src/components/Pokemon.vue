cfee624215299dd9f4c7ab2011d89fae1e1e5307
anapay <anapay42@gmail.com>
2020-08-21 10:18:05 -0300

    add events

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,7 +4,7 @@
     <div class="card">
       <div class="card-image">
         <figure>
-          <img :src="pokemon.front" alt="Placeholder image" />
+          <img :src="currentImg" alt="Placeholder image" />
         </figure>
       </div>
       <div class="card-content">
@@ -14,7 +14,9 @@
             <p class="subtitle is-6">{{pokemon.type}}</p>
           </div>
         </div>
-        <div class="content"></div>
+        <div class="content">
+            <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar Sprite</button>
+        </div>
       </div>
 
     </div>
@@ -34,11 +36,15 @@ export default {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
       console.log(this.pokemon);
     });
   },
   data() {
     return {
+        isFront: true,
+        currentImg: '',
+
       pokemon: {
           type: '',
           front: '',
@@ -58,6 +64,18 @@ export default {
       var newName = value[0].toUpperCase() + value.slice(1);
       return newName;
     }
+  },
+  methods: {
+      mudarSprite: function() {
+          if(this.isFront){
+              this.isFront = false;
+              this.currentImg = this.pokemon.back;
+          }else{
+              this.isFront = true;
+              this.currentImg = this.pokemon.front;
+          }
+      }
+
   }
 };
 </script>
