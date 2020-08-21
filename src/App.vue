d169541e695746f8bc35f50f0f2a402ac28baeae
anapay <anapay42@gmail.com>
2020-08-21 10:01:14 -0300

    import components Pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke,index) in pokemons" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import Pokemon from "./components/Pokemon";
 export default {
   name: "App",
-  data(){
+  data() {
     return {
       pokemons: []
-    }
-
+    };
   },
   created: function() {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
-      console.log("lista de pokemons");
-      this.pokemons = res.data.results;
-      console.log(this.pokemons);
-      
-      
-    })
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then(res => {
+        console.log("lista de pokemons");
+        this.pokemons = res.data.results;
+        console.log(this.pokemons);
+      });
+  },
+  components: {
+    Pokemon
   }
 };
 </script>
