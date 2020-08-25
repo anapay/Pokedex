f79c5f120cbb332f48c9c626ea3d21d8e47b062c
anapay <anapay42@gmail.com>
2020-08-24 21:08:35 -0300

    deploy

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use('/', serverStatic(path.join(__dirname, + '/dist')))
 
 const port = process.env.PORT || 8080
+
 app.listen(port)
 
 console.log('Listening on port: ' +port)
